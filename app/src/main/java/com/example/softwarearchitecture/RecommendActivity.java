1e24c9dc88b4505817767c90d27a9363ab9017a6
hyeonyukim <pinklady235@naver.com>
2020-11-13 15:22:08 +0900

    Recommend.

--- app/src/main/java/com/example/softwarearchitecture/RecommendActivity.java
+++ app/src/main/java/com/example/softwarearchitecture/RecommendActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class RecommendActivity extends AppCompatActivity {
+
+    ArrayList<String>[] timetable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page6);
+    }
+
+    protected void generate(){
+
     }
 }

78cb7896c97b6215daa360da58b16c348df25e80
hyeonyukim <pinklady235@naver.com>
2020-11-13 16:31:39 +0900

    .

--- app/src/main/java/com/example/softwarearchitecture/ClassSelectActivity.java
+++ app/src/main/java/com/example/softwarearchitecture/ClassSelectActivity.java
@@ -2,7 +2,9 @@ package com.example.softwarearchitecture;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ClassSelectActivity extends AppCompatActivity {
@@ -12,5 +14,10 @@ public class ClassSelectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_select);
 //        TextView a = new TextView("")
+    }
+
+    public void onClickButton(View view) {
+        Intent P7RecommendIntent = new Intent(ClassSelectActivity.this, RecommendActivity.class);
+        startActivity(P7RecommendIntent);
     }
 }

5b02413ec22d413fe229984641bd1278b970377f
hyeonyukim <pinklady235@naver.com>
2020-11-21 21:49:07 +0900

    Working on generating recommend timetable.

--- app/src/main/java/com/example/softwarearchitecture/ClassSelectActivity.java
+++ app/src/main/java/com/example/softwarearchitecture/ClassSelectActivity.java
@@ -17,72 +17,97 @@ import android.widget.TextView;
 import com.example.softwarearchitecture.databinding.ActivityClassSelectBinding;
 import com.example.softwarearchitecture.databinding.ItemBinding;
 
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class ClassSelectActivity extends AppCompatActivity {
     ItemBinding binding;
     ActivityClassSelectBinding parentbinding;
+    ArrayList<String> subjectIDs;
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_select);
         myDBHelper dbHelper = new myDBHelper(getApplicationContext());
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db = dbHelper.getWritableDatabase();
         Cursor result = db.rawQuery("SELECT o.subjectFullID, s.subjectName, s.credit, o.time, o.maxStudent, o.professor\n" +
                 "FROM Subject s, OpenedClass o, Curriculum c\n" +
                 "WHERE s.subjectID = o.subjectSubID\n" +
                 "AND s.subjectID NOT IN (SELECT subjectID From LearnedClass)\n" +
                 "AND s.subjectID = c.subjectID;", null);
         result.moveToFirst();
+        subjectIDs = new ArrayList<String>();
+
         do {
-            String subjectFullID = result.getString(0);
+            final String subjectFullID = result.getString(0);
             String subjectName = result.getString(1);
             int credit = result.getInt(2);
-            String time = result.getString(3);
+            String time = compressTime(result.getString(3));
             int maxStudent = result.getInt(4);
             String professor = result.getString(5);
+            subjectIDs.add(subjectFullID);
 
-            LinearLayout body = (LinearLayout) findViewById(R.id.P7body);
-            LinearLayout item=(LinearLayout)LayoutInflater.from(ClassSelectActivity.this)
+            final LinearLayout body = (LinearLayout) findViewById(R.id.P7body);
+            final LinearLayout item=(LinearLayout)LayoutInflater.from(ClassSelectActivity.this)
                     .inflate(R.layout.item, null);
             TextView title = (TextView)item.findViewById(R.id.ItemtextView1);
-            title.setText(subjectName);
+            TextView subscript = (TextView)item.findViewById(R.id.ItemtextView2);
+            TextView subscript2 = (TextView)item.findViewById(R.id.ItemtextView3);
+            Button deleteButton = (Button)item.findViewById(R.id.ItemNoButton);
+            deleteButton.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    body.removeView(item);
+                    subjectIDs.remove(subjectFullID);
+                }
+            });
+            title.setText(subjectName+"("+subjectFullID+")");
+            subscript.setText("교수:" + professor + ", 정원:" + maxStudent + "명, " + credit + "학점");
+            subscript2.setText("강의 시간: " + time);
             body.addView(item);
-
-
-//            parentbinding = ActivityClassSelectBinding.inflate(getLayoutInflater());
-//            binding = ItemBinding.inflate(getLayoutInflater());
-//            View parent = parentbinding.getRoot();
-//            LinearLayout item = binding.getRoot();
-
-//
-//            body.addView(item);
-//            setContentView(parent);
-//            setContentView(R.layout.activity_class_select);
-
-//            LinearLayout linearLayout = new LinearLayout(this);
-//            linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-//
-//            LinearLayout leftside = new LinearLayout(this);
-//            leftside.setOrientation(LinearLayout.VERTICAL);
-//            leftside.setLayoutParams(new LinearLayout.LayoutParams
-//                    (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
-//            linearLayout.addView(leftside);
-//
-//            LinearLayout rightside = new LinearLayout(this);
-//            rightside.setOrientation(LinearLayout.HORIZONTAL);
-//            leftside.setLayoutParams(new LinearLayout.LayoutParams
-//                    (LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT));
-//            linearLayout.addView(leftside);
-//            Log.i("fullid", subjectFullID);
-//            Log.i("name", subjectName);
-//            Log.i("credit", credit+"");
-//            Log.i("time", time);
-//            Log.i("maxStudent", maxStudent+"");
-
         }while(result.moveToNext());
     }
 
+
     public void onClickButton(View view) {
+        for(int i=0; i<subjectIDs.size(); i++){
+            db.execSQL("INSERT INTO SelectedClass VALUES('"+subjectIDs.get(i)+"')");
+        }
         Intent P7RecommendIntent = new Intent(ClassSelectActivity.this, RecommendActivity.class);
         startActivity(P7RecommendIntent);
+    }
+
+    public String compressTime(String time){
+        String res="";
+        StringTokenizer stk = new StringTokenizer(time, " ");
+        char date = 0;
+        String startTime=null, endTime=null;
+
+        while(stk.hasMoreTokens()){
+            String token1 = stk.nextToken();
+            stk.nextToken();
+            String token2 = stk.nextToken();
+            //처음인 경우
+            if(startTime==null&&endTime==null&&date==0){
+                date = token1.charAt(0);
+                startTime = token1.substring(1);
+                endTime = token2;
+            }
+            else if(date==token1.charAt(0) && endTime.equals(token1.substring(1))){
+                endTime = token2;
+            }
+            else{
+                res = res + date + startTime + "~" + endTime + " ";
+                date = token1.charAt(0);
+                startTime = token1.substring(1);
+                endTime = token2;
+            }
+        }
+        res = res + date + startTime + "~" + endTime + " ";
+//        Log.i("res", res);
+//        Log.i("time", time);
+        return res;
     }
 }
--- app/src/main/java/com/example/softwarearchitecture/RecommendActivity.java
+++ app/src/main/java/com/example/softwarearchitecture/RecommendActivity.java
@@ -3,59 +3,103 @@ package com.example.softwarearchitecture;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.sql.Time;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class RecommendActivity extends AppCompatActivity {
 
-    Timetable[] timetable;
+    ArrayList<Timetable>[] timetable;
+    final int maxRecommend = 100;
+    int timetableCnt;
+    TextView[] text;
     int idx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page6);
-        generate();
-        idx=1;
+        timetableCnt = generate();
+        idx=0;
         TextView index = (TextView)findViewById(R.id.P6index);
-        //index.setText(idx+"/"+timetable.length);
-        index.setText(idx+"/1000");
-        //setTimetable(idx);
+        index.setText((idx+1)+"/"+timetableCnt);
+//        index.setText((idx+"/1000");
+        setTimetable(idx);
+    }
+//    db.execSQL("CREATE TABLE Subject (subjectID TEXT PRIMARY KEY, subjectName TEXT, " +
+//         "credit INTEGER);");
+//    db.execSQL("CREATE TABLE OpenedClass (subjectFullID TEXT PRIMARY KEY, subjectSubID TEXT, " +
+//            "gubun TEXT, professor TEXT, time TEXT, classroom TEXT, maxStudent INTEGER, " +
+//            "FOREIGN KEY(subjectSubID) REFERENCES Subject(subjectID));");
+//    db.execSQL("CREATE TABLE SelectedClass(subjectFullID TEXT PRIMARY KEY, " +
+//                "FOREIGN KEY(subjectFullID) REFERENCES OpenedClass(subjectFullID));");
+    protected int generate(){
+        timetable = new ArrayList[maxRecommend];
+        timetable[0] = new ArrayList<Timetable>();
+        myDBHelper dbHelper = new myDBHelper(getApplicationContext());
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.rawQuery("SELECT o.subjectFullID, s.subjectName, o.time, s.credit, o.professor, o.maxStudent " +
+                "FROM SelectedClass c, OpenedClass o, Subject s " +
+                "WHERE c.subjectFullID = o.subjectFullID " +
+                "AND o.subjectSubID = s.subjectID;", null);
+        cursor.moveToFirst();
+        for(int i=0; i<cursor.getCount(); i++){
+            String fullID = cursor.getString(0);
+            String name = cursor.getString(1);
+            String time = compressTime(cursor.getString(2));
+            int credit = cursor.getInt(3);
+            String professor = cursor.getString(4);
+            int maxStudent = cursor.getInt(5);
+            timetable[0].add(new Timetable(fullID, name, time, credit, professor, maxStudent));
+            cursor.moveToNext();
+        }
+        return 1;
     }
 
     public void setTimetable(int index){
         TableLayout tableLayout =(TableLayout)findViewById(R.id.P6table);
+        //tableLayout.setStretchAllColumns(false);
 
-        for(int j=0; j<timetable.length; j++) {
+        if(text!=null){
+            for(int i=0; i<text.length; i++) {
+                tableLayout.removeView(text[i]);
+            }
+        }
+
+        for(int j=0; j<timetable[index].size(); j++) {
             TableRow row = new TableRow(getApplicationContext());
-            TextView[] text = new TextView[6];
-            Timetable subject = timetable[j];
+            text = new TextView[6];
+            Timetable subject = timetable[index].get(j);
 
             for(int i=0; i<text.length; i++) {
                 text[i] = new TextView(getApplicationContext());
                 text[i].setGravity(Gravity.CENTER);
                 text[i].setBackground(getDrawable(R.drawable.tableborder));
+//                text[i].setLayoutParams(new LinearLayout.LayoutParams(
+//                                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
             }
-            //내용 채우기
-            String year = subject.getYear();
-            text[0].setText(""+ year +"");
-            String distinct = subject.getDistinct();
-            text[1].setText(""+ distinct +"");
-            String code =subject.getCode();
-            text[2].setText(""+ code +"");
+            String fullID = subject.getCode();
             String name = subject.getName();
-            text[3].setText(""+ name +"");
+            String time = subject.getTime();
             int credit = subject.getCredit();
-            text[4].setText(""+ credit +"");
-            String score = subject.getScore();
-            text[5].setText(""+ score +"");
+            String professor = subject.getProfessor();
+            int maxStudent = subject.getMaxStudent();
+            text[0].setText(""+ fullID +"");
+            text[1].setText(""+ name +"");
+            //text[2].setText(""+ time +"");
+            text[3].setText(""+ credit +"");
+            text[4].setText(""+ professor +"");
+            text[5].setText(""+ maxStudent +"");
 
             for(int i=0; i<text.length; i++) {
                 row.addView(text[i]);
@@ -65,118 +109,67 @@ public class RecommendActivity extends AppCompatActivity {
         }
     }
 
-    protected void generate(){
-
-    }
-
     public void onClickButton(View view) {
         TextView index = (TextView)findViewById(R.id.P6index);
-        if(view.getId()==R.id.P6nextButton&&idx<timetable.length){
+        if(view.getId()==R.id.P6nextButton&&idx<timetableCnt-1){
             idx++;
         }
-        if(view.getId()==R.id.P6previousButton&&idx>1){
+        if(view.getId()==R.id.P6previousButton&&idx>0){
             idx--;
         }
         index.setText(idx+"/"+timetable.length);
         setTimetable(idx);
     }
 
-    //DB를 생성하고 초기화하는 DB생성자 정의
-    public static class myDBHelper extends SQLiteOpenHelper {
-        public myDBHelper(Context context){
-            super(context, "groupDB", null, 39);
-        }
-        @Override
-        public void onCreate(SQLiteDatabase db){
-            //groupTBL이라는 테이블이름으로 gName, gNumber 필드를 생성해주자
-            db.execSQL("PRAGMA foreign_keys=ON");
-            db.execSQL("CREATE TABLE Student (sID INTEGER PRIMARY KEY, College TEXT, " +
-                    "Major TEXT, Name TEXT, Grage INTEGER, Average_Score REAL);");
-            db.execSQL("CREATE TABLE Subject (subjectID TEXT PRIMARY KEY, subjectName TEXT, " +
-                    "credit INTEGER);");
-            db.execSQL("CREATE TABLE OpenedClass (subjectFullID TEXT PRIMARY KEY, subjectSubID TEXT, " +
-                    "gubun TEXT, professor TEXT, time TEXT, classroom TEXT, maxStudent INTEGER, " +
-                    "FOREIGN KEY(subjectSubID) REFERENCES Subject(subjectID));");
-            db.execSQL("CREATE TABLE LearnedClass (subjectID TEXT, sID INTEGER, " +
-                    "yearSemester TEXT, gubun TEXT, score TEXT, " +
-                    "FOREIGN KEY(subjectID) REFERENCES Subject(subjectID), " +
-                    "FOREIGN KEY(sID) REFERENCES Student(sID), " +
-                    "PRIMARY KEY(subjectID, sID, yearSemester));");
-            db.execSQL("CREATE TABLE Curriculum (subjectID TEXT, sID INTEGER, " +
-                    "yearSemester TEXT, " +
-                    "FOREIGN KEY(subjectID) REFERENCES Subject(subjectID), " +
-                    "PRIMARY KEY(subjectID, sID));");
-            db.execSQL("CREATE TABLE SelectedClass(subjectFullID TEXT PRIMARY KEY, " +
-                    "FOREIGN KEY(subjectFullID) REFERENCES OpenedClass(subjectFullID));");
-        }
+    public String compressTime(String time){
+        String res="";
+        StringTokenizer stk = new StringTokenizer(time, " ");
+        char date = 0;
+        String startTime=null, endTime=null;
 
-        @Override
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-            //이곳에선 테이블이 존재하면 없애고 새로 만들어준다.
-            db.execSQL("DROP TABLE IF EXISTS Student");
-            db.execSQL("DROP TABLE IF EXISTS Subject");
-            db.execSQL("DROP TABLE IF EXISTS OpenedClass");
-            db.execSQL("DROP TABLE IF EXISTS LearnedClass");
-            db.execSQL("DROP TABLE IF EXISTS Curriculum");
-            db.execSQL("DROP TABLE IF EXISTS SelectedClass");
-            onCreate(db);
+        while(stk.hasMoreTokens()){
+            String token1 = stk.nextToken();
+            stk.nextToken();
+            String token2 = stk.nextToken();
+            //처음인 경우
+            if(startTime==null&&endTime==null&&date==0){
+                date = token1.charAt(0);
+                startTime = token1.substring(1);
+                endTime = token2;
+            }
+            else if(date==token1.charAt(0) && endTime.equals(token1.substring(1))){
+                endTime = token2;
+            }
+            else{
+                res = res + date + startTime + "~" + endTime + "\n";
+                date = token1.charAt(0);
+                startTime = token1.substring(1);
+                endTime = token2;
+            }
         }
+        res = res + date + startTime + "~" + endTime + "\n";
+//        Log.i("res", res);
+//        Log.i("time", time);
+        return res;
     }
 }
-
-//    String year = learn_table.getString(0);
-//            text[0].setText(""+ year +"");
-//                    String distinct = learn_table.getString(1);
-//                    text[1].setText(""+ distinct +"");
-//                    String code = learn_table.getString(2);
-//                    text[2].setText(""+ code +"");
-//                    String name = learn_table.getString(3);
-//                    text[3].setText(""+ name +"");
-//                    int credit = learn_table.getInt(4);
-//                    text[4].setText(""+ credit +"");
-//                    String score = learn_table.getString(5);
-//                    text[5].setText(""+ score +"");
-
 class Timetable{
-    String year;
-    //교과구분
-    String distinct;
     String code;
+    //교과구분
     String name;
+    String time;
     int credit;
-    String score;
-    public Timetable(String year, String distinct, String code, String name,
-                     int credit, String score){
+    String professor;
+    int maxStudent;
+
+    public Timetable(String code, String name, String time,
+            int credit, String professor, int maxStudent){
         this.code = code;
         this.credit = credit;
-        this.distinct = distinct;
         this.name = name;
-        this.score = score;
-        this.year = year;
-    }
-
-    public int getCredit() {
-        return credit;
-    }
-
-    public void setCredit(int credit) {
-        this.credit = credit;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public String getDistinct() {
-        return distinct;
-    }
-
-    public void setDistinct(String distinct) {
-        this.distinct = distinct;
+        this.professor = professor;
+        this.time = time;
+        this.maxStudent = maxStudent;
     }
 
     public String getCode() {
@@ -195,11 +188,35 @@ class Timetable{
         this.name = name;
     }
 
-    public String getScore() {
-        return score;
+    public String getTime() {
+        return time;
     }
 
-    public void setScore(String score) {
-        this.score = score;
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    public String getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(String professor) {
+        this.professor = professor;
+    }
+
+    public int getMaxStudent() {
+        return maxStudent;
+    }
+
+    public void setMaxStudent(int maxStudent) {
+        this.maxStudent = maxStudent;
     }
 }
